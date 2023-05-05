20deabec06c8d1c7eb02a0765756a182783efc95
Santiago <s.jimenezv10gmail.com>
2023-05-05 17:29:43 -0500

    CORS HABILITADO

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require("express");
 const app = express();
 const routerApi = require("./src/routes");
 const port = 3000;
+const cors = require('cors');
 
 app.listen(port, () => console.log("Active port", port));
 
 app.get("/", (req, res) => {
   res.status(200).send("Deployment");
 });
+
+app.use(cors({
+  origin: '*'
+}));
 
 app.use(express.json());
 
